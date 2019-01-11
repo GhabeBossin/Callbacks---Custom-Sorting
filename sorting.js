72bd14372adcb4e61931f62d7bc18db4396ec7c1
Gabrielle Bossin <gee.bossin@gmail.com>
2019-01-11 02:38:35 +0000

    adding start of function framework

--- sorting.js
+++ sorting.js
@@ -1,5 +1,33 @@
-var array = [10, 2, 5, 1, 9];
-array.sort(function(a, b){
-   return a - b;
-});
-console.log(array);
+
+// .sort numerically exersize 
+
+// var array = [10, 2, 5, 1, 9];
+// array.sort(function(a, b){
+//    return a - b;
+// });
+// console.log(array);
+
+// Advanced sorting exersice
+//Say we have a list of students that we would like to sort in alphabetical order. If students have the same name, then we prioritize the older student first.
+
+//Write a sorting function (sometimes called a custom comparator), that sorts the above array first by the name ascending alphabetically, and in cases where the names are equal sort by descending age.
+
+var students = [
+   { id: 1, name: "bruce",    age: 40 },
+   { id: 2, name: "zoidberg", age: 22 },
+   { id: 3, name: "alex",     age: 22 },
+   { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a, b){
+   students.forEach(function(name) {
+      var nameA = a.name.toUpperCase(); 
+      var nameB = b.name.toUpperCase(); 
+      // if (a === b) {
+      //    return 0;
+      // }
+      // if (a > b) {
+      //    return ;
+      // }
+   })
+})
